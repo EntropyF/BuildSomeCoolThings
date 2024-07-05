a627f7b43953ab572c76319b6a7425ea56579a13
EntropyF <91456302+EntropyF@users.noreply.github.com>
2024-07-05 13:17:17 +0100

    Update WordCloud.py

--- WordCloud.py
+++ WordCloud.py
@@ -3,11 +3,22 @@ from wordcloud import WordCloud
 
 # Sample text
 text = """
-Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects. Python is dynamically typed and garbage-collected. It supports multiple programming paradigms, including structured (particularly, procedural), object-oriented, and functional programming.
+Description: TIPR has built out an Indicator Validation \\
+Pipeline to evaluate threat intelligence IOCs, more specifically \\
+to discern whether an indicator is a TP or FP based on product \\
+telemetry. They are looking to build and improve their pipelines, \\
+to eliminate hard-coded, arbitrary thresholds, and incorporate \\
+SONAR product telemetry to build high fidelity validation pipelines.​
 """
 
 # Generate word cloud
-wordcloud = WordCloud(width=800, height=400, background_color='black', colormap='viridis', contour_color='white', contour_width=3).generate(text)
+# wordcloud = WordCloud(width=800, height=400, background_color='black',
+#                       colormap='viridis', contour_color='white',
+#                       contour_width=3).generate(text)
+
+wordcloud = WordCloud(width = 800, height = 400,
+                background_color ='white',
+                min_font_size = 10).generate(text)
 
 # Display the word cloud using matplotlib
 plt.figure(figsize=(10, 5))
